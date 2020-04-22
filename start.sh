06ef4e31abc2b2a10f9741bbf5508f879506de68
alexberryman <alex.berryman@55places.com>
2020-04-22 01:57:14 -0500

    broken php-fpm app permissions

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@ docker-compose up -d;
 cd ../http-app;
 docker-compose up -d;
 cd ../prefabbed-app;
+docker-compose build php-fpm
 docker-compose up -d;
 docker-compose exec php-fpm composer install;
 docker-compose exec php-fpm vendor/bin/phinx migrate;
 docker-compose exec php-fpm vendor/bin/phinx seed:run;
+sudo chmod 777 -R .
 docker-compose exec php-fpm vendor/bin/prefab;
+sudo chmod 777 -R .
