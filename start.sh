2bbb2fc74cbc293ec081ce69cbc3ee8305f6e882
alexberryman <alex.berryman@55places.com>
2020-04-22 03:31:47 -0500

    /var/www/html/app permissions are still mixed

--- start.sh
+++ start.sh
@@ -3,11 +3,10 @@ docker-compose up -d;
 cd ../http-app;
 docker-compose up -d;
 cd ../prefabbed-app;
-docker-compose build php-fpm
+docker-compose build php-fpm;
 docker-compose up -d;
 docker-compose exec php-fpm composer install;
 docker-compose exec php-fpm vendor/bin/phinx migrate;
 docker-compose exec php-fpm vendor/bin/phinx seed:run;
-sudo chmod 777 -R .
 docker-compose exec php-fpm vendor/bin/prefab;
-sudo chmod 777 -R .
+docker-compose exec php-fpm ls -hal;
--- stop.sh
+++ stop.sh
@@ -2,5 +2,8 @@ cd ./http-app;
 docker-compose down;
 cd ../prefabbed-app;
 docker-compose down;
+docker volume rm prefabbedapp_app;
+sudo chmod 777 -R .;
+git clean -dxf;
 cd ../traefik;
 docker-compose down;
