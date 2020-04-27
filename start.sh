07b2191f1ce9b42dfcadc604fa632fdfa8688de7
Alex Berryman <alex.berryman@55places.com>
2020-04-27 15:48:41 -0500

    dnsmasq for macOS

--- start.sh
+++ start.sh
@@ -3,6 +3,16 @@ cd ./traefik;
 docker-compose up -d;
 # View http://localhost:8080/dashboard/#/ to see all the routers and services
 
+# Setup a resolver for *.localhost domain if on macOS
+if [[ $OSTYPE == darwin* ]]; then
+  sudo mkdir -p /etc/resolver;
+  sudo tee /etc/resolver/localhost >/dev/null <<EOF
+nameserver 127.0.0.1
+EOF
+  cd ../dnsmasq;
+  docker-compose up -d
+fi
+
 # Launch a basic web service since it's simpler to understand than nginx+PHP-FPM
 cd ../http-app;
 docker-compose up -d;
--- stop.sh
+++ stop.sh
@@ -5,8 +5,14 @@ docker-compose down;
 # Stop and cleanup the prefab generated files
 cd ../prefabbed-app;
 docker-compose down;
-docker volume rm prefabbedapp_app;
-git clean -dxf;
+#docker volume rm prefabbedapp_app;
+#git clean -dxf;
+
+
+if [[ $OSTYPE == darwin* ]]; then
+  cd ../dnsmasq;
+  docker-compose down
+fi
 
 # Stop and clean up the Traefik deployment
 cd ../traefik;
